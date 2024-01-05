8cbf4c3a407cfbf0e8e49d03eee1e8d613ee8169
Christian <cjwinkler0@gmail.com>
2024-01-05 18:38:19 -0500

    adjust observer threshold

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
           }
         });
       },
-      { threshold: 0.2 },
+      { threshold: 0.1 },
     );
 
     if (aboutRef.current) observer.observe(aboutRef.current);
