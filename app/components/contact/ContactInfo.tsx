dc1eaca4189e00d091cb1ff787bb7d23cb7442f8
Christian <cjwinkler0@gmail.com>
2024-01-14 13:39:34 -0500

    tighten up contact section

--- app/components/contact/ContactInfo.tsx
+++ app/components/contact/ContactInfo.tsx
@@ -9,7 +9,6 @@ const ContactInfo = () => {
       className={`min-h-[700px] h-fit justify-evenly flex flex-col py-4 px-6 w-full`}
     >
       <h2 className="text-3xl font-semibold text-primary">Contact Info</h2>
-      <div className="border-b border-secondary mx-auto w-1/2"></div>
       <div className="">
         <p className="text-xl font-semibold ">Christian Winkler</p>
         <p className=" text-secondary mb-4">Front-End Developer</p>
@@ -18,26 +17,29 @@ const ContactInfo = () => {
         </a>
         <p className="text-secondary">North Augusta, SC - USA</p>
       </div>
+      <div className="border-b border-secondary mx-auto w-1/2 lg:w-1/3 opacity-50"></div>
+      <p className="h-2 text-accent">Check out my Github profile!</p>
+
       <a
         href="https://github.com/cwinkdev"
-        className="flex justify-center items-center text-2xl bg-base w-fit p-2 px-4 mx-auto rounded-lg border border-base3"
+        className="flex justify-center items-center text-xl bg-base w-fit p-2 px-4 mx-auto rounded-lg border border-base3"
       >
         <SiGithub />
-        <p className="ml-2 my-auto ">cwinkdev</p>
+        <p className="ml-2 mb-1">cwinkdev</p>
       </a>
-      <div className="border-b border-secondary mx-auto w-1/2 my-4"></div>
+
+      <div className="border-b border-secondary mx-auto w-1/2 lg:w-1/3 opacity-50"></div>
       <div className="flex flex-col items-center">
         <a
           href="/path-to-resume.pdf"
           download
-          className="flex justify-center items-center text-xl bg-base2 w-fit p-2 px-4 mx-auto rounded-lg border border-base3"
+          className="flex justify-center items-center text-xl bg-base w-fit p-2 px-4 mx-auto rounded-lg border border-base3"
         >
           <GiCloudDownload />
-          <p className="ml-2">Download resume</p>
+          <p className="ml-2 mb-1">Download resume</p>
         </a>
       </div>
-      <div className="border-b border-secondary mx-auto w-1/2 my-4"></div>
-      <p className="text-primary">Thanks for taking a look!</p>
+      <p className="text-accent">Thanks for taking a look!</p>
     </div>
   );
 };
